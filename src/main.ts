1a0f416bf22e33241c6e7cadbdab5ad41d740dee
Joe Brown <joebrown.4576@gmail.com>
2021-11-13 12:42:36 +0000

    Add eslint and prettier

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import fetch from "node-fetch";
 import { ResponseData } from "./types/types";
 import dotenv from "dotenv";
@@ -45,45 +45,47 @@ interface AllLatLongs {
 
 const getData = async (): Promise<ResponseData | null> => {
   console.log("getting data... :)");
-  const url = `http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/all?res=hourly&key=${process.env.MET_OFFICE_KEY}`
+  const url = `http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/all?res=hourly&key=${process.env.MET_OFFICE_KEY}`;
   const request = await fetch(url);
-  const result = await request.json() as ResponseData;
+  const result = (await request.json()) as ResponseData;
   return result || null;
-}
+};
 
 const getAllDataExclusingWx = async () => {
   const result = await getData();
   return result ? result.SiteRep.DV : null;
-}
+};
 
 const getAllLatLongs = async () => {
   const result = await getData();
-  return result 
-    ? {latLongArray: result.SiteRep.DV.Location.map((l) => {
-      return {name: l.name, lat: l.lat, long: l.lon} as LatLong
-    })} as AllLatLongs
-    : null
-}
+  return result
+    ? ({
+        latLongArray: result.SiteRep.DV.Location.map((l) => {
+          return { name: l.name, lat: l.lat, long: l.lon } as LatLong;
+        }),
+      } as AllLatLongs)
+    : null;
+};
 
 const getLocationById = async (id: string) => {
   const result = await getData();
-  return result 
-    ? result.SiteRep.DV.Location.find((l) => l.i === id) ?? "Location not found" 
-    : "Location not found"
-}
+  return result
+    ? result.SiteRep.DV.Location.find((l) => l.i === id) ?? "Location not found"
+    : "Location not found";
+};
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.send(await getAllDataExclusingWx());
-})
+});
 
 app.get("/latlongs", async (req, res) => {
   res.send(await getAllLatLongs());
-})
+});
 
 app.get("/location/:id", async (req, res) => {
-res.send(await getLocationById(req.params.id));
-})
+  res.send(await getLocationById(req.params.id));
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
-})
+});
